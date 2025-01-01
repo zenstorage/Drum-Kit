ec4c202eb8c1a0b27d513d2ccebc8285e1472a70
zenstorage <bielhenriquek.k@gmail.com>
2025-01-01 17:56:45 -0300

    - Remove backslash in paths

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,15 @@
 function activateKey(ev) {
     const KEY = ev.code;
     const KEYS = {
-        'KeyA': { id: 'KeyA', path: '/src/sounds/clap.mp3' },
-        'KeyS': { id: 'KeyS', path: '/src/sounds/hihat.mp3' },
-        'KeyD': { id: 'KeyD', path: '/src/sounds/kick.mp3' },
-        'KeyF': { id: 'KeyF', path: '/src/sounds/openhat.mp3' },
-        'KeyG': { id: 'KeyG', path: '/src/sounds/boom.mp3' },
-        'KeyH': { id: 'KeyH', path: '/src/sounds/ride.mp3' },
-        'KeyJ': { id: 'KeyJ', path: '/src/sounds/tom.mp3' },
-        'KeyK': { id: 'KeyK', path: '/src/sounds/snare.mp3' },
-        'KeyL': { id: 'KeyL', path: '/src/sounds/bass.mp3' },
+        'KeyA': { id: 'KeyA', path: 'src/sounds/clap.mp3' },
+        'KeyS': { id: 'KeyS', path: 'src/sounds/hihat.mp3' },
+        'KeyD': { id: 'KeyD', path: 'src/sounds/kick.mp3' },
+        'KeyF': { id: 'KeyF', path: 'src/sounds/openhat.mp3' },
+        'KeyG': { id: 'KeyG', path: 'src/sounds/boom.mp3' },
+        'KeyH': { id: 'KeyH', path: 'src/sounds/ride.mp3' },
+        'KeyJ': { id: 'KeyJ', path: 'src/sounds/tom.mp3' },
+        'KeyK': { id: 'KeyK', path: 'src/sounds/snare.mp3' },
+        'KeyL': { id: 'KeyL', path: 'src/sounds/bass.mp3' },
     };
     const KEY_SOUND = KEYS[KEY];
 
